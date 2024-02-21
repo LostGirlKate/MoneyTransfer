09331b4af3e5c418f09abf14130c0d684ae5a176
Lost Girl <ekaterina_kryukova90@mail.ru>
2024-02-21 15:21:40 +0300

    add card type

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,95 @@
+enum class CardType {
+    MASTERCARD, MIR, VISA
+}
+
+const val DAY_LIMIT = 150_000
+const val MONTH_LIMIT = 600_000
+const val MONTH_LIMIT_MASTERCARD_NO_COMMISSION = 75_000
+const val PERCENT_MASTERCARD = 0.6
+const val PERCENT_VISA = 0.75
+const val MASTERCARD_ADD_COMMISSION = 20
+const val VISA_MIN_COMMISSION = 35
+var totalMonthTransferMastercard = 0
+var totalMonthTransferVisa = 0
+var totalMonthTransferMir = 0
+var totalDayTransferMastercard = 0
+var totalDayTransferVisa = 0
+var totalDayTransferMir = 0
+
 fun main() {
-    val amount = 10540
-    val minCommission = 35
-    val commissionPercent = 0.75
-    val commission = Integer.max(minCommission, Math.round(amount * (commissionPercent / 100)).toInt())
-    println("Размер комиссии $commission руб.")
+    transferByCardType(CardType.MASTERCARD, 150_000)
+    transferByCardType(CardType.MASTERCARD, 150_000)
+
+    transferByCardType(CardType.VISA, 100_000)
+    transferByCardType(CardType.VISA, 50_000)
+    transferByCardType(CardType.VISA, 100_000)
+
+    transferByCardType(CardType.MIR, 100_000)
+    transferByCardType(CardType.MIR, 50_000)
+    transferByCardType(CardType.MIR, 50_000)
+}
+
+fun transferByCardType(cardType: CardType, amount: Int) {
+    val totalMonthTransfer = getTotalMonthTransfer(cardType)
+    val totalDayTransfer = getTotalDayTransfer(cardType)
+    if (totalDayTransfer + amount > DAY_LIMIT) {
+        println("Дневной лимит по карте ${cardType.name} исчерпан, вы не можете осуществить перевод на сумму $amount руб!")
+    } else if (totalMonthTransfer + amount > MONTH_LIMIT) {
+        println("Месячный лимит по карте ${cardType.name} исчерпан, вы не можете осуществить перевод на сумму $amount руб.!")
+    } else {
+        val commission = getCommission(cardType, amount, getTotalMonthTransfer(cardType))
+        println("Размер комиссии по карте ${cardType.name} - $commission руб.")
+        applyTransfer(cardType, amount)
+        println("Перевод осуществлен!")
+    }
+    println()
+
+}
+
+fun getCommission(cardType: CardType, amount: Int, totalMonthTransfer: Int): Int {
+    val commission = when (cardType) {
+        CardType.MASTERCARD -> Math.round(
+            (amount - 0.coerceAtLeast(MONTH_LIMIT_MASTERCARD_NO_COMMISSION - totalMonthTransfer))
+                    * (PERCENT_MASTERCARD / 100)
+                    + MASTERCARD_ADD_COMMISSION
+        ).toInt()
+        CardType.VISA -> Integer.max(VISA_MIN_COMMISSION, Math.round(amount * (PERCENT_VISA / 100)).toInt())
+        CardType.MIR -> 0
+    }
+    return commission
+}
+
+fun getTotalDayTransfer(cardType: CardType): Int {
+    return when (cardType) {
+        CardType.MASTERCARD -> totalDayTransferMastercard
+        CardType.VISA -> totalDayTransferVisa
+        CardType.MIR -> totalDayTransferMir
+    }
+}
+
+fun getTotalMonthTransfer(cardType: CardType): Int {
+    return when (cardType) {
+        CardType.MASTERCARD -> totalMonthTransferMastercard
+        CardType.VISA -> totalMonthTransferVisa
+        CardType.MIR -> totalMonthTransferMir
+    }
+}
+
+fun applyTransfer(cardType: CardType, amount: Int) {
+    when (cardType) {
+        CardType.MASTERCARD -> {
+            totalMonthTransferMastercard += amount
+            totalDayTransferMastercard += amount
+        }
+
+        CardType.VISA -> {
+            totalMonthTransferVisa += amount
+            totalDayTransferVisa += amount
+        }
+
+        CardType.MIR -> {
+            totalMonthTransferMir += amount
+            totalDayTransferMir += amount
+        }
+    }
 }
